fea44f5ffa6dabeaabf2cb23c2242052f88b7f48
Iva Babukova <iva.babukova@skyscanner.net>
2016-12-17 10:28:39 +0000

    Added an all different except 0 constraint for the flights.

--- TPSolver/src/main/Solver.java
+++ TPSolver/src/main/Solver.java
@@ -1,6 +1,8 @@
 package main;
 
 import java.util.ArrayList;
+import org.chocosolver.solver.Model;
+import org.chocosolver.solver.variables.IntVar;
 
 /**
  * Created by ivababukova on 12/16/16.
@@ -10,6 +12,8 @@ public class Solver {
 
     ArrayList<Airport> airports;
     ArrayList<Flight> flights;
+    Model model;
+    IntVar[] S;
     int T;
 
     public Solver(ArrayList<Airport> as, ArrayList<Flight> fs, int T){
@@ -18,7 +22,41 @@ public class Solver {
         this.T = T;
     }
 
-    void solve(){
-        System.out.println("I am not implemented yet ");
+    private void init(){
+        this.model = new Model("TP Solver");
+        this.S = model.intVarArray("Flights Schedule", flights.size(), 0, flights.size());
+        IntVar z = model.intVar("End of schedule", 1, flights.size()-1);
+
+        for(int i = 0; i < flights.size()-1; i++) {
+            model.ifThen(
+                    model.arithm(z, "=", i),
+                    model.arithm(S[i], "=", 0)
+            );
+            model.ifThen(
+                    model.arithm(S[i], "=", 0),
+                    model.arithm(S[i+1], "=", 0)
+            );
+        }
+        this.model.allDifferentExcept0(S).post();
+
+        // todo the dep and arr airports of two consecutive flights must be the same
+
+        // todo the first flight must be from the home point
+
+        // todo the last flight must be to the home point
+
+        // todo the duration + time + conn time constraint
+
+        // todo the total time of the schedule must be less or equal to T
+
+    }
+
+    public void solve(){
+        init();
+        model.getSolver().solve();
+        for (IntVar s: S ){
+            System.out.print(s.getValue() + " ");
+        }
+        System.out.println("\nThat's it!");
     }
 }
